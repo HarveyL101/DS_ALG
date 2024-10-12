eeb38cb9273502f8d19a3a3c4cf32886ce970131
Harvey Lopez <up2118183@myport.ac.uk>
2024-10-12 16:09:42 +0100

    completed dupe.js and started + finished selection.js in folder 2

--- 1/dupe.js
+++ 1/dupe.js
@@ -4,19 +4,62 @@ this using the alternative ADT?
 (Example: Convert “abbccd” to “ad”, “dsallasg” to “dg”, and “abccbadd” toanempty string.) */
 
 
-let x = 0;
-let y = 1;
+//code for stack
 
-let str = "abbccd";
+class Stack {
+    constructor() {
+        this.items = [];
+        this.max_size = 5;
+    }
+    push(x) {
+        if (this.items.length == this.max_size) {
+            console.log("Cannot add to a full stack");
+            return;
+        } else {
+            console.log(`${x} has been added to the stack`);
+            return this.items.push(x);
+        }
+    }
+    pop() {
+        if (this.items.length == 0) {
+            console.log("Cannot remove from an empty stack");
+            return;
+        } else {
+            console.log(`${this.items[this.items.length - 1]} has been popped`)
+            return this.items.pop();
+             
 
-let arr = str.split("");
-
-for (let i = 0; i < arr.length; i++) {
-    for (let j = i+1; j < arr.length; j++)
-    if (arr[i] === arr[j]) {
-        arr.pop[i];
-        console.log(arr);
+        }
+    }
+    peek() {
+        if (this.items.length !== 0) {
+            console.log(`The top value of the stack is ${this.items[this.items.length - 1]}`);
+            return this.items[this.items.length - 1];
+        } else {
+            console.log("Cannot peek at empty stack");
+            return;
+        }
+    }
+    length() {
+        console.log(`The height of the stack is ${this.items.length}`);
+        return this.items.length;
     }
 }
 
+const st = new Stack;
+
+let str = "abbccd";
+
+const arr = Array.from(str.split(""));
+
 console.log(arr);
+
+for (let i=0; i<arr.length; i++) {
+    if(st.items.length == 0 || st.items[st.items.length -1] !== arr[i]) {
+        st.items.push(arr[i]);
+    }
+}
+
+console.log(`String with duplicates removed: ${st.items.join('')}`);
+
+
---
+++ 2/selection.js
@@ -0,0 +1,59 @@
+let arr = [];
+
+function init(array, max_size, range) {
+
+    //fills array to its max capacity with any whole numbers within range (e.g. 20)
+    for (let i=0; i < max_size; i++) {
+
+        array.push(Math.floor(Math.random() * range));
+    }
+
+    console.log(`The size of the random array is ${array.length}`);
+    console.log(`Before sorting, the array is ordered by: ${array}`);
+
+    return this.items;
+}
+
+function sort(array) {
+    if (array.length < 2) {
+
+        //standard error message
+        console.log("Cannot sort an empty array, please use the 'init()' function first.");
+        return;
+
+    } else {
+
+        const n = array.length;
+
+        for (let i = 0; i < n - 1; i++) {
+
+            let min_index = i;
+
+            for (let j = i + 1; j < n; j++) {
+                
+                //find smallest index within unsorted part of array
+                if (array[j] < array[min_index]) {
+                    
+                    //updates smallest index if a smaller element is found
+                    min_index = j;
+
+                }
+            }
+
+            //smallest is found, now to swap values of current index and minimum
+            let temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
+        }
+
+        return array;
+    }
+}
+
+
+
+
+init(arr, 20, 30);
+sort(arr);
+
+console.log(`The array in it's sorted format is: ${arr}`);
