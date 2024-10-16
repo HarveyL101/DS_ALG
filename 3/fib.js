df17e464a43c37ede1f36ee383285cf1f900d2fe
Harvey Lopez <up2118183@myport.ac.uk>
2024-10-16 16:23:50 +0100

    BINARY SEARCH FINISHED AND SORTED

--- 3/fib.js
+++ 3/fib.js
@@ -5,17 +5,21 @@ let arr = [];
 arr[0] = 0;
 arr[1] = 1;
 
+//change n to search for a specific nth term
 const n = 8;
 
 const fib1 = n => {
 
+    let x;
     for (let i = 2; i <= n; i++) {
 
         arr[i] = arr[i - 1] + arr[i - 2];
         console.log(`${arr[i]}`);
     }
 
-    return arr[n];
+    console.log(`The ${n}th value of the fibonacci sequence is: ${arr[arr.length - 1]}.`);
+
+    return;
 }
 
 fib1(n);
